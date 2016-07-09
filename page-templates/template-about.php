9da231e48716ca338579bbc362783f5bcb93b9cf
Nick Harbaugh <njharb@gmail.com>
2016-07-09 17:49:12 -0500

    Add Sass; Code cleanup

---
+++ page-templates/template-about.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: About */ ?>
+
+<?php get_header(); ?>
+
+<div class="page-content">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/about-1.jpg">
+        </div>
+
+        <div class="col-md-6">
+            <h1 class="post-title">About</h1>
+            <p>Ross Harbaugh is Professor of Cello at the University of Miami's Frost School of Music, and cellist of the Bergonzi String Quartet. His distinguished teachers include Janos Starker, Peter Howard and Leonard Rose in the United States and Andre Navarra at the Paris Conservatory, as well as chamber music study with the Juilliard Quartet.</p>
+            <p>As a founding member of the New World Quartet he won the Naumburg Prize, a Prix du Disque, and recorded 20 records and CD’s for Vox, MCI Classic and IMP Masters, CRI, Centaur, Fleur de Son, and Musical Heritage labels. He was also instrumental in establishing the Blodgett Concert Series at Harvard.</p>
+            <p>Harbaugh has appeared in concerts at the Kennedy Center, Alice Tully Hall, and Wigmore Hall in London, and has concertized with such artists as Leonard Rose, Bill Preucil, Richard Goode, Jeffrey Kahane, Raphael Hillyer, Joel Krosnick, Jerome Rose, Gil Kalish, and the Guarneri Quartet.</p>
+            <p>He has soloed with the Atlanta Symphony, Cincinnati and Toledo Symphonies, among many others, and appears nationally in recital each year. His teaching experience includes stints at Harvard University, University of Michigan, and Interlochen, with master classes at Yale and Brown Universities.</p>
+            <p>Mr. Harbaugh presents workshops in ensemble communication, and gives cello workshops throughout the country at such venues as the 7th American Cello Congress in Tempe and the ASTA National Conventions. He has authored numerous articles for American String Teacher, The Strad, and is Editor of the American String Teacher’s Chamber Music Forum. He has served as a judge for the Fischoff Chamber music Competition, Stulberg Competition, and Fulbright Awards for Graduate Study Abroad.</p>
+            <p>He has performed and taught at Cabrillo, Interlochen, the Margess Institute in Switzerland, the Castleman Quartet Program, Green Lake Chamber Music Festival, and ORFEO Music Festival in Vipiteno, Italy.</p>
+        </div>
+    </div>
+</div>
+
+<?php
+// get_sidebar();
+get_footer();
